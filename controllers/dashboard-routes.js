42921e7de0fb68ba767931162d406c04e01f08bd
S1NGS1NG80 <160927065+S1NGS1NG80@users.noreply.github.com>
2024-07-25 10:24:15 -0400

    added to the dashboard-routes.js

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -71,4 +71,24 @@ router.get("/new", (req, res) => {
   res.render("new-post");
 });
 
+router.get('/', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: {
+        userId: req.session.user_id,
+      },
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render('dashboard', {
+      dashboard: true,
+      posts,
+      loggedIn: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
